630d1ce9a9df7029a4502f68f79ce2c69fb3d4fb
Ballesito <davidballester@paucasesnovescifp.cat>
2021-11-10 20:51:12 +0100

    He corregido el Issue 1.
    He corregido el Issue que tenia, solo era que habia un metodo que no usaba en DeleteDialog.java,lo he borrado y ya esta.

--- OpusList/TestGson/src/testgson/DeleteDialog.java
+++ OpusList/TestGson/src/testgson/DeleteDialog.java
@@ -42,28 +42,6 @@ public class DeleteDialog extends javax.swing.JDialog {
             }         
         });
     }
-
-    
-    
-    private ImageIcon resizeImageIcon (BufferedImage originalImage, int desiredWidth, int desiredHeight) {
-        int newHeight = 0;    
-        int newWidth = 0;
-        float aspectRatio = (float)originalImage.getWidth() / originalImage.getHeight();
-        if (originalImage.getWidth() > originalImage.getHeight()) {
-            newWidth = desiredWidth;
-            newHeight = Math.round( desiredWidth / aspectRatio);                
-        } else {
-            newHeight = desiredHeight;
-            newWidth = Math.round(desiredHeight * aspectRatio);
-        }
-        
-        Image resultingImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
-        BufferedImage outputImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
-        outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
-        ImageIcon imageIcon = new ImageIcon(outputImage);
-        
-        return imageIcon;
-    }
     
     /**
      * This method is called from within the constructor to initialize the form.
